888b136fb58fce2a3ad94cea48e3f0235985ae42
douglas <douglasarthurhalljr@gmail.com>
2021-02-21 14:24:20 -0500

    another one yo

--- pypoll/main.py
+++ pypoll/main.py
@@ -4,13 +4,13 @@ import csv
 
 election_data = os.path.join(r"C:\Users\dougl\OneDrive\Desktop\election_data.csv")
 
-#set lists for variables
+# lists to capture our variables
 candidates = []
 
 num_votes = []
-
+ 
 percent_votes = []
-
+ 
 total_votes = 0
 
 with open(election_data, newline = "") as csvfile:
@@ -21,8 +21,12 @@ with open(election_data, newline = "") as csvfile:
         # Add to our vote-counter 
         total_votes += 1 
 
-        #
-      
+        '''
+        If the candidate is not on our list, add his/her name to our list, along with 
+        a vote in his/her name.
+        If he/she is already on our list, we will simply add a vote in his/her
+        name 
+        '''
         if row[2] not in candidates:
             candidates.append(row[2])
             index = candidates.index(row[2])
@@ -37,12 +41,20 @@ with open(election_data, newline = "") as csvfile:
         percentage = round(percentage)
         percentage = "%.3f%%" % percentage
         percent_votes.append(percentage)
-        
-          # Find the winning candidate
+    
+    # Find the winning candidate
     winner = max(num_votes)
     index = num_votes.index(winner)
     winning_candidate = candidates[index]
-    
-    
-  
+
+# Displaying results
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {str(total_votes)}")
+print("--------------------------")
+for i in range(len(candidates)):
+    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
+print("--------------------------")
+print(f"Winner: {winning_candidate}")
+print("--------------------------")
 
