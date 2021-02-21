6a3f0cabd6148896c4dc25815771808ecb926882
douglas <douglasarthurhalljr@gmail.com>
2021-02-21 13:02:48 -0500

    work

---
+++ pypoll/main.py
@@ -0,0 +1,42 @@
+#import libraries
+import os
+import csv
+
+election_data = os.path.join(r"C:\Users\dougl\OneDrive\Desktop\election_data.csv")
+
+#set lists for variables
+candidates = []
+
+num_votes = []
+
+percent_votes = []
+
+total_votes = 0
+
+with open(election_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        # Add to our vote-counter 
+        total_votes += 1 
+
+        #
+      
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            num_votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            num_votes[index] += 1
+    
+    # Add to percent_votes list 
+    for votes in num_votes:
+        percentage = (votes/total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        percent_votes.append(percentage)
+    
+  
+
